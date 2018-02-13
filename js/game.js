16d15da6ce683f2f5d5ebe700a104458aaeee506
Daniel <daniel.lenhart@ert.com>
2018-02-13 14:59:14 +0100

    update license

--- js/game.js
+++ js/game.js
@@ -1,10 +1,9 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2018, Daniel Lenhart,The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license
 (function() {
 'use strict';
 /**
- * T-Rex runner.
+ * Sina Heart Lioness Runner
  * @param {string} outerContainerId Outer containing element id.
  * @param {Object} opt_config
  * @constructor
